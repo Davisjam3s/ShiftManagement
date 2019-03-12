e3c1e5e7530ac7ac69b7c33e88664409ad6e4b1a
James Davis <davisjam3s@gmail.com>
2019-03-12 00:59:44 +0000

    Clean up
    Small Clean up, folders

--- index.php
+++ index.php
@@ -8,55 +8,7 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="scripts/LoginChecks.js"></script>
-	<style type="text/css">
-		.LoginForm, .ClockInForm{
-			padding: 10px;
-			background: #ff69b4;
-			text-align: center;
-			font-family: cursive;
-		}
-		td{
-			text-align: center;
-		}
-		body
-		{
-		background-image: url("back1.jpg");
-		}
-		.MainMenuBackGround{
-			background-color: black;
-    		position: absolute;
-    		width: 50vmin;
-    		height: 25vmin;
-    		top: 50%;
-    		left: 50%;
-    		transform: translate(-50%, -50%);
-    		opacity: 0.3;
-    		color: black;
-		}
-		.MainMenu{
-			position: absolute;
-    		width: 50vmin;
-    		height: 25vmin;
-    		top: 50%;
-    		left: 50%;
-    		transform: translate(-50%, -50%);
-		}
-		.MainMenuItem, #MainMenuItem{
-			width: 50%;
-			float: left;
-			height: 50%;
-			text-align: center;
-			line-height: 100px;
-			-webkit-box-shadow:inset 0px 0px 0px 2px black;
-   			-moz-box-shadow:inset 0px 0px 0px 2px black;
-    		box-shadow:inset 0px 0px 0px 2px black;
-    		cursor: pointer;
-		}
-		.MainMenuItem:hover{
-			background-color: black;
-			color: white;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="indexCSS.css">
 	<script>
 		$(document).ready(function() {
 		$("#LoginButton").click(function() {
