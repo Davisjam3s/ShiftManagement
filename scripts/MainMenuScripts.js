3a12393caa75c3eb3941d5dddbd86decf2a8c23d
James Davis <davisjam3s@gmail.com>
2019-03-12 01:09:36 +0000

    code clean up
    clean code

---
+++ scripts/MainMenuScripts.js
@@ -0,0 +1,14 @@
+		$(document).ready(function() {
+		$("#LoginButton").click(function() {
+		$('#ColleagueNumberLogin').show();
+		$('.MainMenuItem').hide();
+		$('.ColleagueNumberLoginNoCode').show();
+		});
+	});
+		$(document).ready(function() {
+		$("#ClockInButton").click(function() {
+		$('#ColleagueNumberClockIn').show();
+		$('.MainMenuItem').hide();
+		$('#ClockInConfirm').show();
+		});
+	});
